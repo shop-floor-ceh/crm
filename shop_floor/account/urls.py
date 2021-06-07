ebd65d81121f78c9110854e6acc054e7700c30a7
yeezy_na_izi <howone1sky@gmail.com>
2021-06-07 13:53:53 +0300

    add dropdown menu with authorization and registration add registration view append this view in urls

--- shop_floor/account/urls.py
+++ shop_floor/account/urls.py
@@ -4,4 +4,5 @@ from account import views
 
 urlpatterns = [
     path('login', views.authorization, name='login_page'),
+    path('registration', views.registration, name='registration_page'),
 ]
--- shop_floor/account/views.py
+++ shop_floor/account/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def authorization(request):
     return render(request, 'login.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
