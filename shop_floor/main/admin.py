c9d4490e5a5330f65eb69d4bf5c5f2fba8577d03
yeezy_na_izi <howone1sky@gmail.com>
2021-06-13 05:45:55 +0300

    apps.py add verbose_name field models.py add 4 models for Participant, Group, Project, Date
    Participant:
    Bundle with one Account
    Name of role
    
    Group:
    Bundle with many Participant
    
    Project:
    Name of project
    synopsis
    literary script
    directors script
    kpp
    open_to_join
    ended
    admin bundle with Account
    bundle with group
    
    Date:
    bundle with project
    visiting_date
    address
    setting
    scene

--- shop_floor/main/admin.py
+++ shop_floor/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Project, Date, Participant, Group
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Date)
+admin.site.register(Group)
+admin.site.register(Participant)
--- shop_floor/main/models.py
+++ shop_floor/main/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from account.models import Account
+
 
 # Create your models here.
+class Participant(models.Model):
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+    participant = models.ForeignKey(verbose_name='Участник', to=Account, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Имя роли', max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    class Meta:
+        verbose_name = 'Команда'
+        verbose_name_plural = 'Команды'
+
+    participants = models.ManyToManyField(
+        verbose_name='Участники группы',
+        to=Participant,
+        blank=True
+    )
+
+
+class Project(models.Model):
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    name = models.CharField(verbose_name='Имя проекта', max_length=120)
+    synopsis = models.FileField(
+        verbose_name='Синопис',
+        upload_to=f'main/static/projects/{name}',
+        blank=True
+    )
+    literary_script = models.FileField(
+        verbose_name='Литературный сценарий',
+        upload_to=f'main/static/projects/{name}',
+        blank=True
+    )
+    directors_script = models.FileField(
+        verbose_name='Режиссерский сценарий',
+        upload_to=f'main/static/projects/{name}',
+        blank=True
+    )
+    kpp = models.FileField(
+        verbose_name='Календарно-постановочный план',
+        upload_to=f'main/static/projects/{name}',
+        blank=True
+    )
+    open_to_join = models.BooleanField(verbose_name='Открыт для входа', default=True)
+    ended = models.BooleanField(verbose_name='Завершен', default=False)
+    admin = models.ForeignKey(verbose_name='Админ', to=Account, on_delete=models.CASCADE)
+    group = models.OneToOneField(verbose_name='Команда', to=Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Date(models.Model):
+    class Meta:
+        verbose_name = 'Дата встречи'
+        verbose_name_plural = 'Даты встреч'
+
+    project = models.ForeignKey(verbose_name='Проект', to=Project, on_delete=models.CASCADE)
+    visiting_date = models.DateTimeField(verbose_name='Дата и время встречи')
+    address = models.CharField(verbose_name='Адресс', max_length=250)
+    setting = models.CharField(verbose_name='Сеттинг', max_length=250)
+    scene = models.TextField(verbose_name='Сцены')
+
+    def __str__(self):
+        return f'{self.project}, {str(self.visiting_date).split()[0]}'
