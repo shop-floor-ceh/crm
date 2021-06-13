75b12b22060c549698db5a4b09eb04dc989874ae
yeezy_na_izi <howone1sky@gmail.com>
2021-06-13 09:14:13 +0300

    replace templates folder to root path

--- shop_floor/account/views.py
+++ shop_floor/account/views.py
@@ -3,6 +3,8 @@ from .forms import CreateUserForm, LoginUserForm
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from account.models import Account
+import os
+from shop_floor.settings import BASE_DIR
 
 
 def profile_page(request):
@@ -10,9 +12,9 @@ def profile_page(request):
         messages.error(request, 'Вы еще не вошли')
         return redirect('/login')
     user = Account.objects.get(username=request.user.username)
-    user.photo.name = '/'.join(user.photo.name.split('/')[1:])
+    user.photo.name = '/'.join(user.photo.name.split('/')[2:])
     context = {'user': user}
-    return render(request, 'profile.html', context)
+    return render(request, os.path.join(str(BASE_DIR) + '/templates/account/', 'profile.html'), context)
 
 
 # def profile_plug(request):
@@ -33,7 +35,7 @@ def authorization(request):
             return redirect('/profile')
         else:
             messages.error(request, 'Такого пользователя не найдено')
-    return render(request, 'login.html', context)
+    return render(request, os.path.join(str(BASE_DIR) + '/templates/account/', 'login.html'), context)
 
 
 def registration(request):
@@ -48,7 +50,7 @@ def registration(request):
             user = form.save()
             login(request, user)
             return redirect('/profile')
-    return render(request, 'registration.html', context)
+    return render(request, os.path.join(str(BASE_DIR) + '/templates/account/', 'registration.html'), context)
 
 
 def logout_def(request):
--- shop_floor/main/views.py
+++ shop_floor/main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from main.models import Project
+from shop_floor.settings import BASE_DIR
+import os
 
 
 def main_page(request):
-    return render(request, 'home.html')
+    return render(request, os.path.join(str(BASE_DIR) + '/templates/main/', 'home.html'))
 
 
 def open_project(request):
     project = Project.objects.filter(open_to_join=True)
     context = {'projects': project}
-    return render(request, 'open_projects.html', context)
+    return render(request, os.path.join(str(BASE_DIR) + '/templates/main/', 'open_projects.html'), context)
