92c818c4c773a73a2b20f66fb9806e12c15ca669
yeezy_na_izi <howone1sky@gmail.com>
2021-06-21 13:40:53 +0300

    now users cant receive 2 equal notification

--- shop_floor/main/utils.py
+++ shop_floor/main/utils.py
@@ -19,7 +19,8 @@ class SendNotificationThread(threading.Thread):
                 now = datetime.datetime.now()
                 for date in dates:
                     notify_date = date.visiting_date - datetime.timedelta(7)
-                    if today > notify_date and int(now.strftime('%H')) == int(date.visiting_time.strftime('%H')) - 3:
+                    if today > notify_date or (today == notify_date and
+                                               int(now.strftime('%H')) == int(date.visiting_time.strftime('%H')) - 3):
                         message_title = 'Напоминалка для смен'
                         message_text = f'У тебя смена {date.visiting_date} в {date.visiting_time.strftime("%H:%M")}\n' \
                                        f'Проект: {date.project}\n' \
@@ -27,23 +28,27 @@ class SendNotificationThread(threading.Thread):
                                        f'Сеттинг: {date.setting}\n' \
                                        f'Сцены: {date.scene}'
                         participants = date.project.participants.all()
+                        users = set()
                         for participant in participants:
-                            message_text = f'Привет, {participant.participant}\n' + message_text
-                            if participant.participant.telegram_notify:
+                            users.add(participant.participant)
+                        for participant in users:
+                            message_text = f'Привет, {participant}\n' + message_text
+                            if participant.telegram_notify:
                                 bot = telebot.TeleBot('1890824375:AAErbP1HmkhidoXqYlhWlYx1iLivGd98ZXE')
                                 try:
-                                    bot.send_message(participant.participant.telegram_id, message_text)
+                                    bot.send_message(participant.telegram_id, message_text)
                                 except:
                                     bot.send_message(
                                         date.project.admin.telegram_id,
-                                        f'Не смог отпрваить сообщение пользователю {participant.participant}'
+                                        f'Не смог отпрваить сообщение пользователю {participant}'
                                     )
-                            if participant.participant.mail_notify:
+                            if participant.mail_notify:
                                 email = EmailMessage(message_title, message_text, 'noreply@semycolon.com',
-                                                     [participant.participant.email], )
+                                                     [participant.email], )
                                 email.send(fail_silently=False)
                         date.notify_send = True
                         date.save()
+
                 time.sleep(300)
         except Exception as e:
             print(e)
