b40a28595154257a6f9ae18bd3e0468de21eabe8
yeezy_na_izi <howone1sky@gmail.com>
2021-06-21 12:25:06 +0300

    run thread for notification add telegram and email senders

--- shop_floor/main/utils.py
+++ shop_floor/main/utils.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import telebot
 from main.models import Date
+from django.core.mail import EmailMessage
 
 
 class SendNotificationThread(threading.Thread):
@@ -19,9 +20,30 @@ class SendNotificationThread(threading.Thread):
                 for date in dates:
                     notify_date = date.visiting_date - datetime.timedelta(7)
                     if today > notify_date and int(now.strftime('%H')) == int(date.visiting_time.strftime('%H')) - 3:
-                        participants = date.project.participants
+                        message_title = 'Напоминалка для смен'
+                        message_text = f'У тебя смена {date.visiting_date} в {date.visiting_time.strftime("%H:%M")}\n' \
+                                       f'Проект: {date.project}\n' \
+                                       f'Адресс: {date.address}\n' \
+                                       f'Сеттинг: {date.setting}\n' \
+                                       f'Сцены: {date.scene}'
+                        participants = date.project.participants.all()
                         for participant in participants:
-                            participant.participant
+                            message_text = f'Привет, {participant.participant}\n' + message_text
+                            if participant.participant.telegram_notify:
+                                bot = telebot.TeleBot('1890824375:AAErbP1HmkhidoXqYlhWlYx1iLivGd98ZXE')
+                                try:
+                                    bot.send_message(participant.participant.telegram_id, message_text)
+                                except:
+                                    bot.send_message(
+                                        date.project.admin.telegram_id,
+                                        f'Не смог отпрваить сообщение пользователю {participant.participant}'
+                                    )
+                            if participant.participant.mail_notify:
+                                email = EmailMessage(message_title, message_text, 'noreply@semycolon.com',
+                                                     [participant.participant.email], )
+                                email.send(fail_silently=False)
+                        date.notify_send = True
+                        date.save()
                 time.sleep(300)
         except Exception as e:
             print(e)
